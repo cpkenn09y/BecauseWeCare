8911e983a287fc823ab6e261f5c76cec974a68a8
Kenneth Yu <kenyu@ucdavis.edu>
2014-01-21 00:37:11 -0800

    fixed contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ BecauseWeCare::Application.routes.draw do
 
   get "/bwc", to: "bwc#index", as: :bwc
   get "/bwc/about-us", to: "bwc#about_us", as: :bwc_aboutus
-  get "/bwc/about-us", to: "bwc#contact", as: :bwc_contact
+  get "/bwc/contact", to: "bwc#contact", as: :bwc_contact
 
   get "/ant", to: "ant#index", as: :ant
   get "/ant/about-us", to: "ant#about_us", as: :ant_aboutus
-  get "/ant/about-us", to: "ant#contact", as: :ant_contact
+  get "/ant/contact", to: "ant#contact", as: :ant_contact
 
 end
